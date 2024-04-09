3306990fa9ae821adb8273e42ff02a0f82467ec6
LucasLLimeira <Lucasdllimeira@hotmail.com>
2024-04-09 15:01:13 -0300

    config inicial

---
+++ Node/pass-in node/src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get("/", () => {
+    return "Hello NLW Unite"
+})
+
+app.get("/teste", () => {
+    return "Hello Teste"
+})
+
+app.listen({ port: 3333 }).then(() => {
+    console.log("HTTP server running")
+})
